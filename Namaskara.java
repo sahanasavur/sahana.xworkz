c1b73ac5879ca01e40cb8925dadfa7bd2f7349c4
sahanasavur <72906551+sahanasavur@users.noreply.github.com>
2020-10-16 10:11:08 +0530

    My first commit

---
+++ Namaskara.java
@@ -0,0 +1,15 @@
+class Wish
+{
+	public static void main(String[] system)
+	{
+		System.out.println("Namaskara");
+	}
+}
+class Camera
+{
+	public static void main(String[] cam)
+	{
+			System.out.println("Canon");
+	
+	}
+}
