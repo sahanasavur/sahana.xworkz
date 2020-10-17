e15abc8a61b95d6229d6748cc7febf67ac746b9e
sahanasavur <72906551+sahanasavur@users.noreply.github.com>
2020-10-17 14:20:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Hi
+{
+	public static void main(String[] sys)
+	{
+		System.out.println("Hello");
+		music();
+        drinking();		
+	}
+
+	public static void music()
+	{
+			System.out.println("Namo");
+	}
+    
+	public static void drinking()
+	{
+			System.out.println("Water");
+	}
+}
---
+++ Maincalled.java
@@ -0,0 +1,16 @@
+class You
+{
+	public static void main(String[] sys)
+	{
+		System.out.println("Hello");
+		write();
+		write();
+		write();
+	}
+
+	public static void write()
+	{
+			System.out.println("Mobile");
+	}
+   
+}
