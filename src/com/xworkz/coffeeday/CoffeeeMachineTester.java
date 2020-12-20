0a9e214c13658d6aab65e6968a08b5d44e7ce160
sahanasavur <72906551+sahanasavur@users.noreply.github.com>
2020-12-20 18:42:34 +0530

    Add files via upload

---
+++ src/com/xworkz/coffeeday/CoffeeeMachineTester.java
@@ -0,0 +1,22 @@
+package com.xworkz.coffeeday;
+
+import com.xworkz.coffeeday.appliances.CoffeeMachine;
+import com.xworkz.coffeeday.appliances.Power;
+
+public class CoffeeeMachineTester {
+
+		public static void main(String[] args) {
+
+			CoffeeMachine machine = new CoffeeMachine(new Power());
+
+			boolean success = machine.grind("Civet");
+			if (success) {
+				System.out.println("I know grinding was done");
+
+			} else {
+				System.out.println("Griding not done");
+
+			}
+
+		}
+}
---
+++ src/com/xworkz/coffeeday/appliances/Power.java
@@ -0,0 +1,14 @@
+package com.xworkz.coffeeday.appliances;
+
+public class Power {
+	private int voltage;
+
+	public Power() {
+		System.out.println("init Power");
+	}
+
+	public boolean generatePower() {
+		this.voltage = 200;
+		return true;
+	}
+}
