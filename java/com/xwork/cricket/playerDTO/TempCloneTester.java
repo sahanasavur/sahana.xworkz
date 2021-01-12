30ab5a1f7694098219821e4affffbe0c24c2f378
sahanasavur <72906551+sahanasavur@users.noreply.github.com>
2021-01-12 17:49:37 +0530

    Add files via upload

---
+++ java/com/xwork/cricket/playerDTO/TempCloneTester.java
@@ -0,0 +1,21 @@
+package com.xwork.cricket.playerDTO;
+
+public class TempCloneTester {
+	public static void main(String[] args) throws CloneNotSupportedException {
+		String name = "Virat Kohli";
+		int jersey = 18;
+		String sport = "Cricket";
+
+		PlayerDTO dto = new PlayerDTO();
+		dto.setJerseyNumber(jersey);
+		dto.setName(name);
+		dto.setSport(sport);
+		System.out.println(dto.hashCode());
+
+		PlayerDTO cloned = dto.clone();
+		System.out.println(cloned.hashCode());
+		System.out.println(cloned.getJerseyNumber());
+		System.out.println(cloned.getName());
+		System.out.println(cloned.getSport());
+	}
+}
