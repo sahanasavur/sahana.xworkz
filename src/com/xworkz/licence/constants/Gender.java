94c538922d4e5ddb3af51e1ff3449893b8c3fdbc
sahanasavur <72906551+sahanasavur@users.noreply.github.com>
2021-01-26 21:44:33 +0530

    Add files via upload

---
+++ src/com/xworkz/licence/constants/Gender.java
@@ -0,0 +1,5 @@
+package com.xworkz.licence.constants;
+
+public enum Gender {
+MALE,FEMALE,TRANS;
+}
---
+++ src/com/xworkz/licence/constants/VehicleType.java
@@ -0,0 +1,6 @@
+package com.xworkz.licence.constants;
+
+public enum VehicleType {
+
+	LMV,HEAVY,TWO_WHEELER,THREE_WHEELER
+}
